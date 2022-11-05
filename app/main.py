3c1eda5734afd666b941cb8b8de2a9b5b0ee3071
Sanctus-Peter <sanctuspeter@gmail.com>
2022-11-05 22:09:43 +0100

    added procfile

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from .database import engine
 from .routers import auth, admin, user, view
 from . import models
 
 
-models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(view.router)
 app.include_router(auth.router)
