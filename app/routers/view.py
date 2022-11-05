795790e2e78d16c26eaf951b2ac9fc27625b154f
Sanctus-Peter <sanctuspeter@gmail.com>
2022-11-05 23:40:42 +0100

    added async call

--- app/routers/view.py
+++ app/routers/view.py
@@ -7,7 +7,7 @@ router = APIRouter(tags=["View all complains"])
 
 @router.get("/viewComplaints", response_model=list[schemas.Complain])
 async def view_all_complains_details(db: Session = Depends(database.get_db),
-                               user: int = Depends(oauth2.get_current_user)):
+                                     user: int = Depends(oauth2.get_current_user)):
     is_admin = user.role == "admin"
     if is_admin:
         complains = db.query(models.Complain).all()
